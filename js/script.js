5214948373e5e661e06cbc52a655e27f70506592
John Kasper Svergja <john.k.svergja@gmail.com>
2015-11-04 14:07:43 +0100

    Diverse endringer for slider

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 //document.getElementById("slidewrapper").style.width = (document.getElementsByClassName("content")[0].offsetWidth+4)*3+"px";
 var x=1;
 
-function slide(){
+function slide(a){
+	alert(a);
 	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
 		x++;
 }
