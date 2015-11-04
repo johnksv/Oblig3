2cee298ee89674caff0702af31a870bb5bce5757
Tobias Blegeberg <tobias.blegeberg@gmail.com>
2015-11-04 23:39:42 +0100

    Gjort endringer på alt. Gidder ikke å gå i detalj.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 // document.getElementById("slider").style.width = document.getElementsByClassName("content")[0].offsetWidth+"px"; Settes til 100% i CSS
 //document.getElementById("slidewrapper").style.width = (document.getElementsByClassName("content")[0].offsetWidth+4)*3+"px";
-var x=1;
+var x = 0;
 
-function slide(a){
-	alert(a);
+function slide(){
+	x++;
 	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
-		x++;
+}
+function backslide(){
+	x--;
+	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
+}
+function prob(){
+	x = 0;
+	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
+}
+function los(){
+	x = 1;
+	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
+}
+function pers(){
+	x =2;
+	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
 }
