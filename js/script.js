fe6cc56020c48c0b2811f8fc668c0d5e1f938032
John Kasper Svergja <john.k.svergja@gmail.com>
2015-11-04 13:27:28 +0100

    Støtte for noscript

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 // document.getElementById("slider").style.width = document.getElementsByClassName("content")[0].offsetWidth+"px"; Settes til 100% i CSS
 //document.getElementById("slidewrapper").style.width = (document.getElementsByClassName("content")[0].offsetWidth+4)*3+"px";
+var x=1;
+
+function slide(){
+	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
+		x++;
+}
