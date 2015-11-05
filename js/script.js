b831f321d06cb632a55834d7715d35bf7ed45539
John Kasper Svergja <john.k.svergja@gmail.com>
2015-11-05 10:11:17 +0100

    Masse endringer: Slider funker nå bra på desktop, også med piltaster. Ordnet nav-meny så den ikke går utenfor bredden. Lagt på stil på den kanppen som er aktiv. Ryddet opp i javascriptfilen Lagt inn href på knappene, så de nå kan tabbes på, uten at dette medfører noe bug. Gjort HTML-koden ryddigere. Nå brukes samme funksjon for all sliding.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
-// document.getElementById("slider").style.width = document.getElementsByClassName("content")[0].offsetWidth+"px"; Settes til 100% i CSS
-//document.getElementById("slidewrapper").style.width = (document.getElementsByClassName("content")[0].offsetWidth+4)*3+"px";
 var x = 0;
 
-function slide(){
-	x++;
+function slideTo(a){
+	document.getElementsByTagName("nav")[0].children[x].classList.remove("activeSlide");
+	if(a==0||a==1||a==2){
+		//Side 1, Side 2, Slide 3
+		x=a;
+	}else if (a== -1){
+		//En tilbake
+		x--;
+	}
+
 	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
+	document.getElementsByTagName("nav")[0].children[x].classList.add("activeSlide");
 }
-function backslide(){
-	x--;
-	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
+
+function setStyle(){
+	document.getElementsByTagName("nav")[0].children[x].classList.add("activeSlide");
 }
-function prob(){
-	x = 0;
-	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
-}
-function los(){
-	x = 1;
-	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
-}
-function pers(){
-	x =2;
-	document.getElementById("slidewrapper").style.left = "-"+x+"00%";
-}
+
+
+//Gustav sitt forslag til piltaster
+window.addEventListener('keydown', function(event) {
+    switch (event.keyCode) {
+        case 37: // Left
+            if(x>0){
+            	var temp = x-1;
+            	slideTo(temp);
+            }
+            break;
+
+        case 39: // Right
+        	if(x<2){
+        		var temp = x+1;
+        		slideTo(temp);
+        	}
+            break;
+    }
+}, false);
